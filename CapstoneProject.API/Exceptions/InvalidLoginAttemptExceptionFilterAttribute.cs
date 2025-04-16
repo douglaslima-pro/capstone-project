f05064351d00b8705a3d88e2ff91693ac65fc6ab
douglaslima-pro <douglaslima-pro@outlook.com>
2025-04-16 10:25:18 -0300

    Exception handling

---
+++ CapstoneProject.API/Exceptions/InvalidLoginAttemptExceptionFilterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using CapstoneProject.Business.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CapstoneProject.API.Exceptions
+{
+    public class InvalidLoginAttemptExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is InvalidLoginAttemptException)
+            {
+                ProblemDetails problemDetails = new ProblemDetails
+                {
+                    Title = context.Exception.Message,
+                    Status = (int)HttpStatusCode.Unauthorized,
+                };
+                
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                context.Result = new ObjectResult(problemDetails);
+
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
---
+++ CapstoneProject.API/Exceptions/NotFoundExceptionFilterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using CapstoneProject.Business.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CapstoneProject.API.Exceptions
+{
+    public class NotFoundExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is NotFoundException)
+            {
+                ProblemDetails problemDetails = new ProblemDetails
+                {
+                    Title = context.Exception.Message,
+                    Status = (int)HttpStatusCode.NotFound,
+                };
+
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                context.Result = new ObjectResult(problemDetails);
+
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
